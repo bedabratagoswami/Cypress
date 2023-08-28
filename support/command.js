301b42ce603e0bbcc93765e3977494a79f768788
bedabratagoswami <125532227+bedabratagoswami@users.noreply.github.com>
2023-08-28 10:55:13 +0530

    Update command.js

--- support/command.js
+++ support/command.js
@@ -1,1 +1,27 @@
+var jwt = require('jsonwebtoken');
+const client_iddd = Cypress.env('cypress.env.json')
+Cypress.Commands.add('login', () => {
+  return cy
+    .request({
+      method: 'POST',
+      url: 'https://login.microsoftonline.com/ca2ba8c3-b4b1-4aad-9251-d2dca59fd212/oauth2/token',
+      form: true,
+      body: {
+        grant_type: 'password',
+        tenant: 'ca2ba8c3-b4b1-4aad-9251-d2dca59fd212',
+        client_id: '862ff324-c86a-4a2b-9bd7-30054e8c7ee2',
+        client_secret: 'D6K8Q~stHIIMpjWtHjh7lMZU0ppPwi6gkEC5fcYj',
+        username: 'bedabrata.goswami@weareflexday.com',
+        password: 'Spotty123!',
+        resource: '862ff324-c86a-4a2b-9bd7-30054e8c7ee2',
+      },
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+    })
+    .then((response) => {
+      sessionStorage.setItem('access_token', response.body.access_token);
+    });
+    
+});
 
