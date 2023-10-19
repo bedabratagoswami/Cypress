d717939378db4466687b7648d90cbe0fd5aecbec
bedabratagoswami <125532227+bedabratagoswami@users.noreply.github.com>
2023-10-19 11:17:29 +0530

    Update addLogin.js

--- addLogin.js
+++ addLogin.js
@@ -8,7 +8,6 @@ module.exports = {
     return null;
   },
   async aadLogin(options = {}) {
-    console.log("task triggered :::: ",options)
     const username = options.username;
     const password = options.password;
     const appUrl = options.appUrl;
@@ -29,28 +28,19 @@ module.exports = {
     await page.keyboard.press('Enter');
     await page.waitForNavigation();
     await new Promise(r => setTimeout(r, 2000));
-   await page.waitForSelector(passwordSelector);
-   await page.focus(passwordSelector);
+    await page.waitForSelector(passwordSelector);
+    await page.focus(passwordSelector);
     await page.type(passwordSelector, password);
-    //await page.waitForSelector(forgotSelector);
-    //await page.click(forgotSelector);
     await page.waitForSelector(submitButtonSelector);
     await page.click(submitButtonSelector);
-
     await page.waitForNavigation();
     await page.waitForSelector(submitButtonSelector);
     await page.click(submitButtonSelector);
     await page.waitForNavigation();
-    console.log("GETTING  THE BROWSER LOCAL STORAGE *****************")
     const localStorage = await page.evaluate(() =>  Object.assign({}, window.localStorage));
     await new Promise(r => setTimeout(r, 5000));
-    console.log("GETTING  THE BROWSER SESSION STORAGE *****************")
     const sessionStorage = await page.evaluate(() =>  Object.assign({}, window.sessionStorage));
-    //await new Promise(r => setTimeout(r, 3000));
-    console.log("TRYING TO CLOSE THE BROWSER++++++++++++++")
     await page.close()
-    console.log("BROWER CLOSED*************************")
-    //await browser.close();
     return{localStorage: localStorage, sessionStorage: sessionStorage}
     },
     };
